fd064654f4d2806f1733eeff0b55957fc135d70d
Rom Ogen <romogen2@gmail.com>
2018-11-16 13:04:07 +0200

    refactored the code and ifxed a bug

--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -1,14 +1,12 @@
 import * as esprima from 'esprima';
 
-/* TODO
- * 1. need to handle numerical expression disassemble
-  * */
-
 const createLine = (Line, Type, Name='', Condition='', Value='') => {
     return {'line': Line, 'type': Type, 'name': Name, 'condition': Condition, 'value': Value};
 };
 
 const getOriginalText = (originalText, parsedCodeJSON) => {
+    if(parsedCodeJSON == null) return null;
+
     let startLine = parsedCodeJSON['loc']['start']['line'], startCol = parsedCodeJSON['loc']['start']['column'],
         endLine = parsedCodeJSON['loc']['end']['line'], endCol = parsedCodeJSON['loc']['end']['column'];
     let splitText = originalText.split('\n'), fullTextAns = '';
@@ -25,11 +23,7 @@ const getOriginalText = (originalText, parsedCodeJSON) => {
     return fullTextAns;
 };
 
-const handleElseStatement = (parsedCodeJSON, originalText) => {
-
-};
-
-const handleIfStatement = (parsedCodeJSON, originalText, is_else) => {
+const handleIfStatement = (parsedCodeJSON, originalText, is_else=false) => {
     let ansArr = [];
     ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], ((!is_else)?'if statement':'else if statement'),
         '', getOriginalText(originalText, parsedCodeJSON['test'])));
@@ -45,78 +39,103 @@ const handleIfStatement = (parsedCodeJSON, originalText, is_else) => {
     return ansArr;
 };
 
-const produceTable = (parsedCodeJSON, originalText) => {
+const handleProgram = (parsedCodeJSON, originalText) => {
     let ansArr = [];
-    switch (parsedCodeJSON['type']) {
-    case 'Program':
-        // produce the table recursively for the program body
-        for (let x = 0; x < parsedCodeJSON['body'].length ; x++)
-            ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'][x], originalText));
-        break;
+    for (let x = 0; x < parsedCodeJSON['body'].length ; x++)
+        ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'][x], originalText));
+    return ansArr;
+};
 
-    case 'FunctionDeclaration':
-        // push function declaration
-        ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'function declaration',
-            parsedCodeJSON['id']['name']));
+const handleFunctionDeclaration = (parsedCodeJSON, originalText) => {
+    let ansArr = [];
 
-        // push variable declarations
-        for (let x = 0 ; x < parsedCodeJSON['params'].length ; x++)
-            ansArr.push(createLine(parsedCodeJSON['params'][x]['loc']['start']['line'], 'variable declaration',
-                parsedCodeJSON['params'][x]['name']));
+    // push function declaration
+    ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'function declaration',
+        parsedCodeJSON['id']['name']));
 
-        // parse function body recursively
-        ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
-        break;
+    // push variable declarations
+    for (let x = 0 ; x < parsedCodeJSON['params'].length ; x++)
+        ansArr.push(createLine(parsedCodeJSON['params'][x]['loc']['start']['line'], 'variable declaration',
+            parsedCodeJSON['params'][x]['name']));
 
-    case 'BlockStatement':
-        // if an object and not a series of commands
-        if (!Array.isArray(parsedCodeJSON['body']))
-            return ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
-
-        // if is series of command (eg: is array).
-        for (let x = 0; x < parsedCodeJSON['body'].length ; x++)
-            ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'][x], originalText));
-        break;
-
-    case 'VariableDeclaration':
-        for (let x = 0; x < parsedCodeJSON['declarations'].length ; x++)
-            ansArr.push(createLine(parsedCodeJSON['declarations'][x]['id']['loc']['start']['line'],
-                'variable declaration', parsedCodeJSON['declarations'][x]['id']['name'], '',
-                getOriginalText(originalText, parsedCodeJSON['declarations'][x])));
-        break;
-
-    case 'ExpressionStatement':
-        ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'assignment expression',
-            parsedCodeJSON['expression']['left']['name'], '',
-            getOriginalText(originalText, parsedCodeJSON['expression']['right'])));
-        break;
-
-    case 'WhileStatement':
-        ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'while statement',
-            '', getOriginalText(originalText, parsedCodeJSON['test'])));
-        ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
-        break;
-
-    case 'ForStatement':
-        ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'for statement', '',
-            getOriginalText(originalText, parsedCodeJSON['init']) +
-            getOriginalText(originalText, parsedCodeJSON['test']) + ';' +
-            getOriginalText(originalText, parsedCodeJSON['update'])));
-        ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
-        break;
-
-    case 'IfStatement':
-        ansArr = ansArr.concat(handleIfStatement(parsedCodeJSON, originalText, false));
-        break;
-
-    case 'ReturnStatement':
-        ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'return statement', '', '',
-            getOriginalText(originalText, parsedCodeJSON['argument'])));
-        break;
-
-    }
+    // parse function body recursively
+    ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
 
     return ansArr;
+};
+
+const handleBlockStatement = (parsedCodeJSON, originalText) => {
+    let ansArr = []
+    // if an object and not a series of commands
+    if (!Array.isArray(parsedCodeJSON['body']))
+        return ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
+
+    // if is series of command (eg: is array).
+    for (let x = 0; x < parsedCodeJSON['body'].length ; x++)
+        ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'][x], originalText));
+
+    return ansArr;
+};
+
+const handleVariableDeclaration = (parsedCodeJSON, originalText) => {
+    let ansArr = []
+
+    for (let x = 0; x < parsedCodeJSON['declarations'].length ; x++)
+        ansArr.push(createLine(parsedCodeJSON['declarations'][x]['id']['loc']['start']['line'],
+            'variable declaration', parsedCodeJSON['declarations'][x]['id']['name'], '',
+            getOriginalText(originalText, parsedCodeJSON['declarations'][x]['init'])));
+
+    return ansArr;
+};
+
+const handleExpressionStatement = (parsedCodeJSON, originalText) => {
+    return createLine(parsedCodeJSON['loc']['start']['line'], 'assignment expression',
+        parsedCodeJSON['expression']['left']['name'], '',
+        getOriginalText(originalText, parsedCodeJSON['expression']['right']));
+};
+
+const handleWhileStatement = (parsedCodeJSON, originalText) => {
+    let ansArr = []
+
+    ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'while statement',
+        '', getOriginalText(originalText, parsedCodeJSON['test'])));
+    ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
+
+    return ansArr;
+};
+
+const handleForStatement = (parsedCodeJSON, originalText) => {
+    let ansArr = []
+
+    ansArr.push(createLine(parsedCodeJSON['loc']['start']['line'], 'for statement', '',
+        getOriginalText(originalText, parsedCodeJSON['init']) +
+        getOriginalText(originalText, parsedCodeJSON['test']) + ';' +
+        getOriginalText(originalText, parsedCodeJSON['update'])));
+    ansArr = ansArr.concat(produceTable(parsedCodeJSON['body'], originalText));
+
+    return ansArr;
+};
+
+const handleReturnStatement = (parsedCodeJSON, originalText) => {
+    return createLine(parsedCodeJSON['loc']['start']['line'], 'return statement', '', '',
+        getOriginalText(originalText, parsedCodeJSON['argument']));
+};
+
+const functionsDict = {
+    'Program': handleProgram,
+    'FunctionDeclaration': handleFunctionDeclaration,
+    'BlockStatement': handleBlockStatement,
+    'VariableDeclaration': handleVariableDeclaration,
+    'ExpressionStatement': handleExpressionStatement,
+    'WhileStatement': handleWhileStatement,
+    'ForStatement': handleForStatement,
+    'IfStatement': handleIfStatement,
+    'ReturnStatement': handleReturnStatement
+};
+
+const produceTable = (parsedCodeJSON, originalText) => {
+    let relevantFunction = functionsDict[parsedCodeJSON['type']];
+    return relevantFunction(parsedCodeJSON, originalText);
 };
 
 const parseCode = (codeToParse) => {
